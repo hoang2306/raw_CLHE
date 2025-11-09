6e25520abf9d2335404abb1dd970dfb63be03181
Pham Duc Hoang <hoangggp@gmail.com>
2025-11-09 23:38:10 +0700

    test

--- models/one_view_CLHE.py
+++ models/one_view_CLHE.py
@@ -332,6 +332,7 @@ class CLHE(nn.Module):
         print(f'neg score: {neg_score}')
         print(f'pos score: {pos_score}')
         diff = pos_score - neg_score # [bs, 5]
+        print(f'diff: {diff}')
         bpr = -torch.log(torch.sigmoid(diff) + 1e-8)  # [bs, n_pos]
         print(f'bpr: {bpr}')
         valid_mask = (
