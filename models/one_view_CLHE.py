a24bb86aa46bb13b82eae2df1f4aafe81761c850
hoang2306 <hoangggp@gmail.com>
2025-11-19 09:03:28 +0700

    test

--- models/one_view_CLHE.py
+++ models/one_view_CLHE.py
@@ -141,9 +141,10 @@ class HierachicalEncoder(nn.Module):
         features = [mm_feature_full]
         features.append(self.item_embeddings)
 
-        # cf_feature_full = self.cf_transformation(self.cf_feature)
-        # cf_feature_full[self.cold_indices_cf] = mm_feature_full[self.cold_indices_cf]
-        # features.append(cf_feature_full)
+        if self.conf['use_item_pretrained']:
+            cf_feature_full = self.cf_transformation(self.cf_feature)
+            cf_feature_full[self.cold_indices_cf] = mm_feature_full[self.cold_indices_cf]
+            features.append(cf_feature_full)
 
         features = torch.stack(features, dim=-2)  # [bs, #modality, d]
 
@@ -200,9 +201,10 @@ class HierachicalEncoder(nn.Module):
 
         features.append(self.item_embeddings)
 
-        # cf_feature_full = self.cf_transformation(self.cf_feature)
-        # cf_feature_full[self.cold_indices_cf] = mm_feature_full[self.cold_indices_cf]
-        # features.append(cf_feature_full)
+        if self.conf['use_item_pretrained']:
+            cf_feature_full = self.cf_transformation(self.cf_feature)
+            cf_feature_full[self.cold_indices_cf] = mm_feature_full[self.cold_indices_cf]
+            features.append(cf_feature_full)
 
         features = torch.stack(features, dim=-2)  # [bs, #modality, d]
         size = features.shape[:2]  # (bs, #modality)
