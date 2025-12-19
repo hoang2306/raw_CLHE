5a4beb7923ff4b03bac8b22604ae0166052c9c77
hoang2306 <hoangggp@gmail.com>
2025-12-20 05:53:37 +0700

    test

--- models/one_view_CLHE.py
+++ models/one_view_CLHE.py
@@ -268,7 +268,10 @@ class MoE_Layer(torch.nn.Module):
         
         gate_logits = self.gate(x)
         # add noise to gate logits for exploration
-        noise = torch.randn_like(gate_logits) * 1e-1
+        # 1e-1: good
+
+        # noise = torch.randn_like(gate_logits) * 1e-1
+        noise = torch.randn_like(gate_logits)
         gate_logits = gate_logits + noise
 
         aux_loss = self._compute_load_balancing_loss(gate_logits)
